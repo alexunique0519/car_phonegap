9b7c548566d7e60473d3b0230ff948c2944a070a
Alex Yan <alexunique0519@gmail.com>
2016-01-04 23:49:09 -0500

    ..

--- WWW/app/app.js
+++ WWW/app/app.js
@@ -6,51 +6,17 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
         controller: "homeController",
-        templateUrl: "/app/views/home.html"
+        templateUrl: "./app/views/home.html"
     });
 
     $routeProvider.when("/login", {
         controller: "loginController",
-        templateUrl: "/app/views/login.html"
+        templateUrl: "app/views/login.html"
     });
 
     $routeProvider.when("/signup", {
         controller: "signupController",
-        templateUrl: "/app/views/signup.html"
-    });
-
-    $routeProvider.when("/profile", {
-        controller: "userProfileController",
-        templateUrl: "/app/views/profile.html"
-
-    });
-
-    $routeProvider.when("/associate", {
-        controller: "associateController",
-        templateUrl: "/app/views/associate.html"
-    });
-
-    $routeProvider.when("/activate", {
-        controller: "activateController",
-        templateUrl: "/app/views/activate.html",
-        resolve: {
-            // I will cause a 1 second delay
-            delay: function ($q, $timeout) {
-                var delay = $q.defer();
-                $timeout(delay.resolve, 1000);
-                return delay.promise;
-            }
-        }
-    });
-
-    $routeProvider.when("/begin_password_reset", {
-        controller: "resetPasswordController",
-        templateUrl: "/app/views/begin_password_reset.html"
-    });
-
-    $routeProvider.when("/setNewPassword", {
-        controller: "setNewPasswordController",
-        templateUrl: "/app/views/setNewPassword.html"
+        templateUrl: "views/signup.html"
     });
 
      $routeProvider.when("/tripSearch", {
@@ -59,17 +25,17 @@ app.config(function ($routeProvider) {
     
     $routeProvider.when("/tripResult", {
        controller: "tripResultController",
-       templateUrl: "/app/views/tripSearchResult.html"
+       templateUrl: "views/tripSearchResult.html"
     });
     
     $routeProvider.when("/tripShareView", {
         controller: "tripShareViewController",
-        templateUrl: "/app/views/tripShareView.html"
+        templateUrl: "views/tripShareView.html"
     });
     
     $routeProvider.when("/shareRide", {
         controller: "shareRideController",
-        templateUrl: "/app/views/shareRideView.html"
+        templateUrl: "views/shareRideView.html"
     });
     
    $routeProvider.otherwise({ redirectTo: "/home" });
